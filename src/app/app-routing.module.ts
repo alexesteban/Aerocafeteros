bcd3cb250c6d8866032a069cee2951bb9ac826e6
alexesteban <alexesteban_89@hotmail.es>
2017-06-05 22:35:01 -0500

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IntroComponent } from './intro/intro.component';
+import { HomeComponent } from './home/home.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+const routes: Routes = [
+  { path: 'intro',
+    component: IntroComponent,
+    data: { title: 'Intro' }
+  },
+  { path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  },
+  { path: 'nosotros',
+    component: NosotrosComponent,
+    data: { title: 'Nosotros' }
+  },
+  { path: 'servicios',
+    component: ServiciosComponent,
+    data: { title: 'Servicios' }
+  },
+  { path: 'contacto',
+    component: ContactoComponent,
+    data: { title: 'Contacto' }
+  },
+  { path: '',
+    redirectTo: '/intro',
+    pathMatch: 'full'
+  },
+  { path: '**',
+    redirectTo: '/intro',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
